3d0dc92942e7f61a9d2f6752d0d41101acb40818
alboy77 <al.boucetta@yahoo.com>
2023-04-06 19:43:04 -0400

    gotta get api to work

--- assets/script.js
+++ assets/script.js
@@ -11,11 +11,40 @@ setInterval(displayTime, 1000);
 
 var APIkey = "7d622ed797f96c87522cc2f44e3ebc16";
 
-function getAPI(city) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+if(localStorage.get("city")) {
+    var searchCity = localStorage.getItem("city");
+} else {
+    var searchCity = "Philadelphia";
+};
+
+var searchButton = document.querySelector("#user-form");
+var forecastView = document.querySelector("#forecast");
+function getWeather(data) {
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid=1a6e242c584145cebf5c8827e5e6e268&units=imperial";
 fetch(queryURL)
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
-    .then(function (data) {})
+    .then(function (data) {
+      console.log(data);
+    })
 }
+
+
+
+// function getAPI(city) {
+//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+// fetch(queryURL)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         getWeather(data);
+//         var button = document.createElement("button");
+//         button.className = "btn";
+//         button.textContent = city;
+//         button.onlcick = function() {getAPI(city)};
+//         document.getElementById("city-buttons").appendChild(button);
+//         localStorage.setItem("city", city);
+//     });
+// };
