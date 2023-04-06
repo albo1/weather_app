4fadf2c7ca5dbd1e9c95a170602beb34cac9eebf
alboy77 <al.boucetta@yahoo.com>
2023-04-06 19:17:36 -0400

    added more css, attempting js. v hard

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,12 @@ displayTime ();
 setInterval(displayTime, 1000);
 
 var APIkey = "7d622ed797f96c87522cc2f44e3ebc16";
+
+function getAPI(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {})
+}
