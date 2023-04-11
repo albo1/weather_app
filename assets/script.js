dd93d92ec36772d6b8a7fc0a799f686a38c4beec
alboy77 <al.boucetta@yahoo.com>
2023-04-10 20:05:42 -0400

    localstorage and 5 day

--- assets/script.js
+++ assets/script.js
@@ -8,17 +8,19 @@ function displayTime() {
 
 displayTime();
 setInterval(displayTime, 1000);
+// getWeather(savedCity);
 
 var APIkey = "7d622ed797f96c87522cc2f44e3ebc16";
 var city;
-var searchButton = document.getElementById("searchBtn");
+var searchButton = document.getElementById("search-button");
+var searchBar = document.querySelector(".search-bar");
 var searchInput = document.getElementById("forecast");
 var cityLocation = document.getElementById("city-location");
+var cityInput = document.querySelector("#city-input")
+var savedCity = localStorage.getItem("city");
+var cityList = document.querySelector("#city-list");
+var savedCities = JSON.parse(localStorage.getItem("saveCities")) || [];
 
-
-// cityLocation.innerHTML = data.city.name + " (" + dayjs().format('M/D/YY') + ") <img src='https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png'/>";
-
-// "https://api.openweathermap.org/data/2.5/forecast?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=1a6e242c584145cebf5c8827e5e6e268&units=imperial";
 
 function getWeather(city) {
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
@@ -28,6 +30,8 @@ function getWeather(city) {
     })
     .then(function (data) {
       console.log(data);
+      document.querySelector(".city").textContent = data.name;
+
       var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=1a6e242c584145cebf5c8827e5e6e268&units=imperial";
       fetch(queryURL2)
         .then(function (response) {
@@ -35,7 +39,59 @@ function getWeather(city) {
         })
         .then(function (data) {
           console.log(data);
+          document.querySelector(".temp").textContent = data.list[0].main.temp
+          document.querySelector(".humidity").textContent = data.list[0].main.humidity
+          document.querySelector(".wind").textContent = data.list[0].wind.speed
+
+          var boxOne = document.querySelector("#box1");
+          boxOne.innerHTML = dayjs().add(1, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[8].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[8].main.temp + "°F<br/>Wind: " + data.list[8].wind.speed + " mph<br/>Humidity: " + data.list[8].main.humidity + "%";
+
+          var boxTwo = document.querySelector("#box2");
+          boxTwo.innerHTML = dayjs().add(1, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[8].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[8].main.temp + "°F<br/>Wind: " + data.list[8].wind.speed + " mph<br/>Humidity: " + data.list[8].main.humidity + "%";
+
+          var boxThree = document.querySelector("#box3");
+          boxThree.innerHTML = dayjs().add(3, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[24].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[24].main.temp + "°F<br/>Wind: " + data.list[24].wind.speed + " mph<br/>Humidity: " + data.list[24].main.humidity + "%";
+
+          var boxFour = document.querySelector("#box4");
+          boxFour.innerHTML = dayjs().add(4, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[32].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[32].main.temp + "°F<br/>Wind: " + data.list[32].wind.speed + " mph<br/>Humidity: " + data.list[32].main.humidity + "%";
+
+          var boxFive = document.querySelector("#box5");
+          boxFive.innerHTML = dayjs().add(5, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[39].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[39].main.temp + "°F<br/>Wind: " + data.list[39].wind.speed + " mph<br/>Humidity: " + data.list[39].main.humidity + "%";
+
+
         })
     })
 }
-getWeather("philadelphia");
+
+function saveCityLS(city) {
+  savedCity = city;
+  var cities = JSON.parse(localStorage.getItem("cities")) || [];
+  cities.push(city);
+  localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+savedCities.forEach(city => {
+  const listItem = document.createElement('li');
+  listItem.className = 'list-group-item';
+  listItem.textContent = city;
+
+  listItem.addEventListener("click", () => {
+    getWeather(city);
+  });
+  cityList.appendChild(listItem);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (savedCity) {
+    getWeather(savedCity);
+  }
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  cityInput.value;
+  console.log(cityInput.value);
+  saveCityLS(cityInput.value);
+  getWeather(cityInput.value);
+  savedCity = cityInput.value;
+});
